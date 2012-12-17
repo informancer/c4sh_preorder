768e5ffe383e5765c843d59c155d0692b68575f0
bios <bios@spamers.net>
2012-12-17 03:12:48 +0100

    Added parts of the legal disclaimer back in

--- preorder/app_views.py
+++ preorder/app_views.py
@@ -497,9 +497,8 @@ def print_tickets_view(request, preorder_id, secret):
         pdf.set_y(720)
         pdf.set_right_margin(300)
         
-        #Computer says no
-        #if ticket.price > 0:
-        #    pdf.write(10, "Bis zu einem Ticketpreis von 150,00 EUR gilt das Ticket gleichzeitig als Kleinbetragsrechnung im Sinne von § 33 UStDV. Eine Berechtigung zum Vorsteuerabzug besteht bei einem Ticketpreis von mehr als 150,00 EUR nur in Verbindung mit einer separaten Rechnung. Umtausch und Rueckgabe ausgeschlossen.")
+        if ticket.price > 0:
+            pdf.write(10, "Bis zu einem Ticketpreis von 150,00 EUR gilt das Ticket gleichzeitig als Kleinbetragsrechnung im Sinne von § 33 UStDV.")
     #are Credit Card payments enabled? If yes, is this a preorder paid by CC?
     if settings.EVENT_CC_ENABLE and preorder.paid_via=="creditcard":
         total = preorder.get_sale_amount()[0]['total']
@@ -563,9 +562,6 @@ def print_tickets_view(request, preorder_id, secret):
         pdf.set_font(font, '', 8)
         pdf.set_y(720)
         pdf.set_right_margin(300)
-        #Computer says no
-        #if ticket.price > 0:
-        #    pdf.write(10, "Bis zu einem Ticketpreis von 150,00 EUR gilt das Ticket gleichzeitig als Kleinbetragsrechnung im Sinne von § 33 UStDV. Eine Berechtigung zum Vorsteuerabzug besteht bei einem Ticketpreis von mehr als 150,00 EUR nur in Verbindung mit einer separaten Rechnung. Umtausch und Rueckgabe ausgeschlossen.")
 
     
     response = HttpResponse(mimetype="application/pdf")
