5dada4abbf934fadf6eafe2127704e2ae4661b7f
zakx <sg@unkreativ.org>
2013-06-05 06:09:29 +0200

    updating default logging for Django 1.5

--- c4sh_preorder/settings-dist.py
+++ c4sh_preorder/settings-dist.py
@@ -145,14 +145,23 @@ INSTALLED_APPS = (
 	'south'
 )
 
+if DEBUG:
+	INSTALLED_APPS += ('django_extensions',)
+
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
 	'handlers': {
 		'mail_admins': {
 			'level': 'ERROR',
+			'filters': ['require_debug_false'],
 			'class': 'django.utils.log.AdminEmailHandler'
 		}
 	},
