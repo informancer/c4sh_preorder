12c6b94d80d36fed6f8dcba1f3ba88779389cc34
zakx <sg@unkreativ.org>
2013-06-05 06:37:11 +0200

    django.conf.urls.defaults is deprecated, using django.conf.urls now

--- c4sh_preorder/urls.py
+++ c4sh_preorder/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from c4sh_preorder.settings import MEDIA_ROOT, STATIC_ROOT
 
 from django.contrib import admin
