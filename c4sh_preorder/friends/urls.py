74c2083f91486f5202fb5eecd9330025c43ba0d2
Timo Josten <tj@mkswap.net>
2013-10-25 23:31:42 +0200

    Removing test url name

--- c4sh_preorder/friends/urls.py
+++ c4sh_preorder/friends/urls.py
@@ -5,5 +5,5 @@ from c4sh_preorder.settings import MEDIA_ROOT, STATIC_ROOT
 
 urlpatterns = patterns('c4sh_preorder.friends.views',
     url(r'^$', 'friends_apply', name='friends-apply'),
-    url(r'^b70f1a8fcc5047575f92200122b9a4dc2742aabf/(?P<secret>(\w+))/$', 'friends_review', name='friends-review'),
+    url(r'^review/(?P<secret>(\w+))/$', 'friends_review', name='friends-review'),
 )
