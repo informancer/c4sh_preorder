de70e31213566d6ce73fc1e6af1a6d66f985b947
informancer <informancer@web.de>
2014-02-10 07:15:59 +0000

    Further access tests for admin views

--- c4sh_preorder/backend/tests.py
+++ c4sh_preorder/backend/tests.py
@@ -49,6 +49,86 @@ class AdminDefaultViewTestCase(TestCase):
         self.assertTemplateUsed('admin/default.html')
 
 
+class AdminStatsViewTestCase(TestCase):
+    fixtures = ['users.json']
+    
+    def test_unauthenticated_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('admin-statistics'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics')))
+        response = self.client.post(reverse('admin-statistics'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics')))
+
+    def test_authenticated_user(self):
+        response = self.client.login(username='normaluser', 
+                                     password='test')
+        response = self.client.get(reverse('admin-statistics'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics')))
+        response = self.client.post(reverse('admin-statistics'), follow=True)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics')))
+
+    def test_authenticated_staffuser(self):
+        response = self.client.login(username='staffuser', 
+                                     password='test')
+        response = self.client.get(reverse('admin-statistics'), follow=False)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics.html')
+        response = self.client.post(reverse('admin-statistics'), follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics.html')
+
+    def test_authenticated_superuser(self):
+        response = self.client.login(username='superuser', 
+                                     password='vagrant')
+        response = self.client.get(reverse('admin-statistics'), follow=False)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics.html')
+
+
+class AdminStatsChartsViewTestCase(TestCase):
+    fixtures = ['users.json']
+    
+    def test_unauthenticated_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('admin-statistics-charts'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics-charts')))
+        response = self.client.post(reverse('admin-statistics-charts'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics-charts')))
+
+    def test_authenticated_user(self):
+        response = self.client.login(username='normaluser', 
+                                     password='test')
+        response = self.client.get(reverse('admin-statistics-charts'), follow=False)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics-charts')))
+        response = self.client.post(reverse('admin-statistics-charts'), follow=True)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'),
+                                                           reverse('admin-statistics-charts')))
+
+    def test_authenticated_staffuser(self):
+        response = self.client.login(username='staffuser', 
+                                     password='test')
+        response = self.client.get(reverse('admin-statistics-charts'), follow=False)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics_charts.html')
+        response = self.client.post(reverse('admin-statistics-charts'), follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics_charts.html')
+
+    def test_authenticated_superuser(self):
+        response = self.client.login(username='superuser', 
+                                     password='vagrant')
+        response = self.client.get(reverse('admin-statistics-charts'), follow=False)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('admin/statistics_charts.html')
+
+
 class AdminCsvImportViewTestCase(TestCase):
     fixtures = ['users.json']
     
